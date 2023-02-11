14268abc58faf6812efc0e3a9e072e641a8e79ef
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-02-11 10:44:31 -0800

    Vite app with netlify deploy

---
+++ src/app.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import './app.css'
+import type { FunctionComponent } from 'preact'
+
+export const App: FunctionComponent = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <img src='/vite.svg' class='logo' alt='Vite logo' />
+        </a>
+        <a href='https://preactjs.com' target='_blank' rel='noreferrer'>
+          <img src={preactLogo} class='logo preact' alt='Preact logo' />
+        </a>
+      </div>
+      <h1>Vite + Preact</h1>
+      <div class='card'>
+        <button onClick={() => { setCount((count) => count + 1) }}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class='read-the-docs'>
+        Click on the Vite and Preact logos to learn more
+      </p>
+    </>
+  )
+}
